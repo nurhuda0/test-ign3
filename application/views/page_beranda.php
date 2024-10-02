bd141c8ec97c3d925be51157173c599e37ca836e
nurhuda0 <zerobizzz@gmail.com>
2024-10-02 10:16:04 +0700

    konfigurasi MVC awal

---
+++ application/views/page_beranda.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Beranda</title>
+</head>
+
+<body>
+    <h1>ini adalah halaman beranda dari aplikasi web ini</h1>
+</body>
+
+</html>
